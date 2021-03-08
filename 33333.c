625e3e35bce783a841ec7572382b0b79725d9b82
b6320500549 <jirasin.po@ku.th>
2021-03-08 19:48:54 +0700

    Update 33333.c

--- 33333.c
+++ 33333.c
@@ -1,20 +1,27 @@
 #include<stdio.h>
 int main()
 {
-  int n,m=0,i=1,j=1,h[3]={2,3,5};
+  int n,m=0,i=1,j=0,k=0,h[3]={2,3,5};
   scanf("%d",&n);
-  m=n;
-  while(i>0)
-      {
-        for(j=1;j!=3;)
+   for(i=1;i>0;i++)
+    {
+        m=i;
+        for(j=0;j!=3;)
         {
          if(m%h[j]==0)
             {
-                m=h[j];
+                m/=h[j];
             }
          else j++;
         }
-       i++;
-
+       if(m==1)
+        {
+            k++;
+            if(k==n)
+            {
+                printf("%d",i);
+                break;
+            }
+        }
       }
 }
